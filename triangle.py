fc93366cb2de410da57a729115b1864133f2294d
Kevin <35900784+kxue4@users.noreply.github.com>
2018-03-08 15:34:39 -0500

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -14,10 +14,14 @@ def classify_triangle(a_float, b_float, c_float):
     """
     The function to classify triangle type.
     """
+    if not(isinstance(a_float,(int, float)) and isinstance(b_float,(int, float)) and isinstance(c_float,(int, float))):
+        return 'not valid'
+
     per = a_float + b_float + c_float
     m_float = max(a_float, b_float, c_float)
     n_float = min(a_float, b_float, c_float)
     i_float = per - m_float - n_float
+
     if n_float + i_float <= m_float or n_float <= 0 or m_float >= 200:
         return 'not valid'
 
@@ -36,25 +40,3 @@ def classify_triangle(a_float, b_float, c_float):
         return 'Isosceles'
 
     return 'Scalene'
-
-
-def main():
-    """
-    This is the main() function
-    """
-    try:
-        a_float = float(input('Please enter A: '))
-        b_float = float(input('Please enter B: '))
-        c_float = float(input('Please enter C: '))
-    except ValueError:
-        print('Not valid! Wrong input')
-    else:
-        result = classify_triangle(a_float, b_float, c_float)
-        if result == 'not valid':
-            print('Wrong input, this triangle is not valid!')
-        else:
-            print(classify_triangle(a_float, b_float, c_float), 'triangle.')
-
-
-if __name__ == '__main__':
-    main()
